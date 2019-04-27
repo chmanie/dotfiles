a05bea1ea19dbb4928039a39a3237b03c8964493
Christian Maniewski <chmanie@fastmail.com>
2019-04-27 14:17:30 +0800

    Add Readme

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Install
+
+```
+mkdir -p ~/dev
+git clone git@github.com:chmanie/dotfiles.git ~/dev/dotfile
+```
+
+## Mac
+
+```
+~/dev/dotfiles/macos/install.sh
+```
--- macos/install.sh
+++ macos/install.sh
@@ -1,14 +1,12 @@
 #!/bin/sh 
 
-mkdir -p ~/dev
-
-git clone git@github.com:chmanie/dotfiles.git ~/dev/dotfiles
-git clone https://github.com/chmanie/dotfiles.git ~/dev/dotfiles
-
 # Link the correct shellenv (macos)
 ln -s ~/dev/dotfiles/macos ~/.shellenv
 
+# tmux stuff
 ln -s ~/dev/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dev/dotfiles/.tmux ~/.tmux
+
 ln -s ~/dev/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/dev/dotfiles/.config/alacritty/ ~/.config/alacritty
 
