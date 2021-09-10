5e887e3846966baf02d0f05731314fea48ea49fc
Christian Maniewski <code@chmanie.com>
2021-09-10 15:12:38 +0200

    Add wofi, improve sway statusline

---
+++ .config/sway/status.sh
@@ -0,0 +1,29 @@
+# The Sway configuration file in ~/.config/sway/config calls this script.
+# You should see changes to the status bar after saving this script.
+# If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
+
+# Produces "21 days", for example
+uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
+
+# The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
+# like 2018-10-06 and the time (e.g., 14:01)
+date_formatted=$(date "+%a %F %H:%M")
+
+# Get the Linux version but remove the "-1-ARCH" part
+linux_version=$(uname -r | cut -d '-' -f1)
+
+# "upower --enumerate | grep 'BAT'" gets the battery name (e.g.,
+# "/org/freedesktop/UPower/devices/battery_BAT0") from all power devices.
+# "upower --show-info" prints battery information from which we get
+# the state (such as "charging" or "fully-charged") and the battery's
+# charge percentage. With awk, we cut away the column containing
+# identifiers. i3 and sway convert the newline between battery state and
+# the charge percentage automatically to a space, producing a result like
+# "charging 59%" or "fully-charged 100%".
+battery_info=$(upower --show-info $(upower --enumerate |\
+grep 'BAT') |\
+egrep "state|percentage" |\
+awk '{print $2}')
+
+echo $uptime_formatted \| $linux_version \| $battery_info \| $date_formatted
+
